6bdb8b63e6f31ee234b77320e535a17272df7398
Ferllings <contact@ferllings.me>
2023-05-23 21:23:36 +0200

    [feat] added more options for log file

--- eria.go
+++ eria.go
@@ -22,6 +22,10 @@ var (
 	CoreVersion = "-"
 	_logLevel   = zerolog.InfoLevel
 	_configPath *string
+	_logPath    *string
+	_logFormat  *string
+	_logOutput  *os.File
+	_logNoColor = false
 	_appName    string
 )
 
@@ -31,16 +35,39 @@ func Init(appName string) {
 	//	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	showVersion := flag.Bool("v", false, "Display the version")
 	logLevelStr := flag.String("log", "info", "log level [error, warn, info, debug, trace]")
-	_configPath = flag.String("config", "config.yml", "config file path")
+	_configPath = flag.String("config-path", "config.yml", "config file path")
+	_logPath = flag.String("log-path", "", "log file path")
+	_logFormat = flag.String("log-format", "pretty", "log output format [pretty, json]")
 	flag.Parse()
-
+	var err error
 	// Show version (-v)
 	if *showVersion {
 		fmt.Printf("%s (%s)\n", AppVersion, BuildDate)
 		os.Exit(0)
 	}
 
-	zlog.Logger = zlog.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "02/01|15:04:05"})
+	if *_logPath != "" {
+		_logOutput, err = os.OpenFile(*_logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Printf("Can't set log file '%s': %v\n", *_logPath, err)
+			os.Exit(1)
+		}
+		_logNoColor = true
+		zlog.Trace().Str("file", *_logPath).Msg("[core:Init] Log on file selected")
+	} else {
+		_logOutput = os.Stdout
+		zlog.Trace().Msg("[core:Init] Log on console selected")
+	}
+
+	if *_logFormat == "pretty" {
+		zlog.Logger = zlog.Output(zerolog.ConsoleWriter{Out: _logOutput, NoColor: _logNoColor, TimeFormat: "02/01|15:04:05"})
+	} else if *_logFormat == "json" {
+		zlog.Logger = zlog.Output(_logOutput)
+	} else {
+		fmt.Printf("Can't unknown log format '%s'\n", *_logFormat)
+		os.Exit(1)
+	}
+
 	zerolog.TimestampFunc = func() time.Time {
 		return time.Now().In(time.Local)
 	}
@@ -99,5 +126,11 @@ func WaitForSignal() {
 
 	// Block until keyboard interrupt is received.
 	<-c
-	zlog.Info().Msg("[core:WaitForSignal] Keyboard interrupt received, Stopping...")
+	zlog.Info().Msg("[core:WaitForSignal] Keyboard interrupt received")
+}
+
+// Stop and close all services/files
+func Close() {
+	zlog.Debug().Msg("[core:Close] Closing...")
+	_logOutput.Close()
 }
